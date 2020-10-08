a9f078e6b5d514e8ce4103ad7a42cdea0f7638bd
Actuate Nuxt Deploy <info@actuateinnovation.org>
2020-10-08 04:02:19 +0000

    Site update

---
+++ _nuxt/pages/404-7bb33ae.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{436:function(e,r,t){"use strict";t.r(r);var n={components:{"v-error-page":t(35).a}},o=t(3),component=Object(o.a)(n,(function(){var e=this.$createElement;return(this._self._c||e)("v-error-page",{attrs:{error:{statusCode:404}}})}),[],!1,null,null,null);r.default=component.exports}}]);
